0a16339e4a85bcee84d37328b24a1187513946ef
avi-watwani <avi.watwani@gmail.com>
2022-06-20 17:45:19 +0530

    Sign up labelled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
   get "about", to: "about#index"
+  get "sign_up", to: "registrations#new"
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
